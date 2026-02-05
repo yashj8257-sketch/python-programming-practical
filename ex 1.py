2c65dd0924d2940ca8cd20de0115dbc284bcba5a
yashj8257-sketch <yashj8257@gmail.com>
2026-02-05 11:21:17 +0530

    python programming Ex 1

---
+++ ex 1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 11:00:50 2026
+
+@author: User
+"""
+
+name = input("enter your name :")
+
+age = input(" enter your age : ")
+
+student_class = input ("enter your class : ")
+
+roll_no = input("enter your roll number : ") 
+
+batch = input("enter your batch : ")
+
+college = input("enter your college name : ")
+
+print("\n----student information----")
+
+
